a28a319eaed39c8e7cf0b12843b1ed4cda8a8f39
Aji Suryawan <kazumovie132@gmail.com>
2020-06-26 11:54:15 +0700

    Initial Commit

---
+++ sharekegit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sharekegit
+//
+//  Created by Aji Suryawan on 26/06/20.
+//  Copyright © 2020 RPL RUS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
